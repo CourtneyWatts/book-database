56e4b17726c8bbe352d929cc8166cb03f59dfe3b
CourtneyWatts <courtney.watts@icloudm>
2020-07-09 14:14:45 +0100

    Updates

---
+++ precache-manifest.68a64bc9bb20256ff5fc6ab90ecdd0c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af595fe6e074c5a1475118f12f0c14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b1939a864e3c9268c9",
+    "url": "/static/css/main.2f836f0c.chunk.css"
+  },
+  {
+    "revision": "0949ce7c4e4c1e6ef6eb",
+    "url": "/static/js/2.0c04280c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c04280c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b1939a864e3c9268c9",
+    "url": "/static/js/main.4a04cb87.chunk.js"
+  },
+  {
+    "revision": "c04f05cffdb40f14153f",
+    "url": "/static/js/runtime-main.1aa6bfdc.js"
+  },
+  {
+    "revision": "7266cf10789f09c264ee2423a31b4656",
+    "url": "/static/media/Banner.7266cf10.jpg"
+  }
+]);
